7a682d4e4c98476120538d53861bf9c80901de75
Efaz Hossain <efaz.hossain@mavs.uta.edu>
2024-04-22 21:28:19 -0500

    Bugs occurred but will be fixed l8r

--- Projects/DeepLearning/process.py
+++ Projects/DeepLearning/process.py
@@ -1,40 +1,66 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+
 # In[2]:
 
 
-from datetime import datetime
-import numpy as np
-import numpy as numpy
-import pandas as pd
-import pylab
-import calendar
-from scipy import stats
-import seaborn as sns
-from sklearn import model_selection
-from scipy.stats import kendalltau
-import warnings
-import matplotlib.pyplot as plt
-import pandas
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
+def encode_categorical(X):
+    encoder = OneHotEncoder(drop="first", sparse=False)
+    X_encoded = pd.DataFrame(encoder.fit_transform(X[categorical_cols]))
+    X_encoded.columns = encoder.get_feature_names(categorical_cols)
+    X.drop(columns=categorical_cols, inplace=True)
+    X = pd.concat([X, X_encoded], axis=1)
+    return X
 
-def label_encode(df1,df2):
-    for c in df2.columns: # Converts all categorical values to numerical values via Label Encoding
-        df2[c]=df2[c].fillna(-1)
-        if df2[c].dtype == 'object':
-            lbl = LabelEncoder()
-            lbl.fit(list(df2[c].values))
-            df2[c] = lbl.transform(list(df2[c].values))
-    train_df = df1.merge(df2, how='left', on='parcelid') #Merged the old dataset with the converted properties set
-    return train_df
 
-#### Preprocessing
-def preprocess(train1,test1):
-    # StandardScaler
-    sc = StandardScaler()
-    train1 = sc.fit_transform(train1)
-    test1 = sc.transform(test1)
+# In[3]:
+
+
+def handle_missing_values(data):
+    # Replace missing values with median for numerical columns
+    imputer = SimpleImputer(strategy="median")
+    data[numerical_cols] = imputer.fit_transform(data[numerical_cols])
+    
+    # Replace missing values with most frequent value for categorical columns
+    data[categorical_cols] = data[categorical_cols].fillna(data[categorical_cols].mode().iloc[0])
+    
+    return data
+
+
+# In[4]:
+
+
+def scale_features(X):
+    scaler = StandardScaler()
+    X[numerical_cols] = scaler.fit_transform(X[numerical_cols])
+    return X
+
+
+# In[5]:
+
+
+def preprocess_data(data):
+    # Handling missing values
+    data = handle_missing_values(data)
+    
+    # Splitting features and target variable
+    X = data.drop(columns=["target_column"])
+    y = data["target_column"]
+    
+    # Scaling numerical features
+    X = scale_features(X)
+    
+    # Handling categorical variables
+    X = encode_categorical(X)
+    
+    return X, y
 
